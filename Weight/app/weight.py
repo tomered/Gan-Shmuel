cd37e40a2941196283843d41f0c1d22044e54d34
YuvalMadari <yuvalmadari1@gmail.com>
2025-03-17 18:46:26 +0200

    Flask initialization

--- Weight/app/weight.py
+++ Weight/app/weight.py
@@ -1,13 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, Response, request, jsonify
-import os
-
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/health', methods=['GET'])
-def healthcheck():
-    return ('OK', 200)
-
+@app.route('/')
+def home():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
