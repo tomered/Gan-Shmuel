35af324b049c905cef4983ab28e48f043d6ba133
Tal Koosh <talkoosh@Tals-MacBook-Air.local>
2025-03-17 15:12:28 +0200

    fix db connection

--- Billing/app/app.py
+++ Billing/app/app.py
@@ -1,22 +1,36 @@
 import os 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+import time
+
+def get_db_connection():
+    """Create a connection to the MySQL database"""
+    # Try to connect multiple times (useful when starting up)
+    for _ in range(10):
+        try:
+            conn = mysql.connector.connect(
+                host="db",
+                user="root",
+                password="secret",
+                database="billdb"
+            )
+            return conn
+        except mysql.connector.Error:
+            # Wait and retry
+            time.sleep(3)
+    
+    raise Exception("Could not connect to MySQL database")
+
+
 
 app = Flask(__name__)
 
-db = SQLAlchemy(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-class Provider(db.Model):
-    # TODO: Check if im able to set my own id
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255))
-
 
 if __name__ == '__main__':
-    db.create_all()
     # TODO: Check if host 0.0.0.0 is the correct way to do this
     app.run(host='0.0.0.0', debug=True, port=4000)
+
+
+
+
+
