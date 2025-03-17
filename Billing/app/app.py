92fc09fdd74f41842258c3e8ee51ab9bdb483e78
Tal Koosh <talkoosh@Tals-MacBook-Air.local>
2025-03-17 14:22:18 +0200

    basic connection to db

--- Billing/app/app.py
+++ Billing/app/app.py
@@ -1,11 +1,22 @@
+import os 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+class Provider(db.Model):
+    # TODO: Check if im able to set my own id
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255))
 
 
 if __name__ == '__main__':
+    db.create_all()
+    # TODO: Check if host 0.0.0.0 is the correct way to do this
     app.run(host='0.0.0.0', debug=True, port=4000)
