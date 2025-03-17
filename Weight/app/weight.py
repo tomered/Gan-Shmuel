a26943486f52eb1f191ff89974121d3136441f9b
YuvalMadari <yuvalmadari1@gmail.com>
2025-03-17 18:51:53 +0200

    GET /health implementation

--- Weight/app/weight.py
+++ Weight/app/weight.py
@@ -10,9 +10,21 @@ mydb = mysql.connector.connect(
   database="database"
 )
 
+cursor = mydb.cursor(dictionary=True)
+
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Hello, rom weight server!"
+
+@app.route('/health', methods=['GET'])
+def healthcheck():
+      if mydb.is_connected():
+        cursor.execute("SELECT 1;") 
+        cursor.fetchall()  # Ensure all results are read
+        return "OK", 200
+      else:
+          return "Failure", 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
